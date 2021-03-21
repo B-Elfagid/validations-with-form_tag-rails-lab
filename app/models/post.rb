8f92004e8e556ef0a5046145eb2de892461e8854
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-03-21 12:40:05 +0000

    commit tree 5f1f7c78efade34d01138b5ec40aa8409d5fb429

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, presence: true, length: {minimum: 200}
+    validates :category, presence: true, inclusion: { in: %w(Fiction Non-Fiction) }
 end
